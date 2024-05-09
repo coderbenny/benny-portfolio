96ca76dacb792582aa53dab31244e0d000b81f6f
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-05-09 15:56:44 +0300

    installed react router dom & added basic components

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="">
+      <h2 className="font-bold text-xl">Homepage</h2>
+    </div>
+  );
+}
+
+export default Home;
