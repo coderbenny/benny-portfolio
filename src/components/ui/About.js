b8cfe382613e5421655d925ab4a2d37918ada89c
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-05-10 17:05:06 +0300

    added the about section and tech stack

--- src/components/ui/About.js
+++ src/components/ui/About.js
@@ -7,7 +7,18 @@ function About() {
         About
       </h3>
       <div className="p-6 w-full">
-        <div className="h-[250px] w-full rounded-md shadow-md bg-white"></div>
+        <div className="h-[250px] text-center w-full p-3 rounded-md shadow-md bg-white">
+          <p className="">
+            I am a passionate software engineer who crafts innovative solutions
+            that merge functionality with elegance
+          </p>
+          <br />
+          <p className="mb-7">
+            I am fuelled by the thrill of problem solving and the satisfaction
+            of bringinng ideas to life.
+          </p>
+          <p className="">Nairobi, Kenya.</p>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/Right.js
+++ src/components/ui/Right.js
@@ -5,7 +5,19 @@ import ColorTabs from "../ui/ColorTabs";
 function Right() {
   return (
     <div className="w-3/4 h-full bg-white">
-      <div className="h-[150px] bg-gray-200"></div>
+      <div className="flex flex-col items-center justify-center h-[150px] bg-gray-200 p-2">
+        <h2 className="font-bold text-center text-xl">Tech Stack</h2>
+        <div className="flex justify-between mt-5">
+          <p className="mr-4 text-white rounded-md bg-slate-950 p-1">ReactJs</p>
+          <p className="mr-4 text-white rounded-md bg-slate-950 p-1">
+            Tailwindcss
+          </p>
+          <p className="mr-4 text-white rounded-md bg-slate-950 p-1">Flask</p>
+          <p className="mr-4 text-white rounded-md bg-slate-950 p-1">
+            SQLAlchemy
+          </p>
+        </div>
+      </div>
       <ColorTabs />
     </div>
   );
