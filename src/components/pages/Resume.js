aa8a0792c7403415143e56c5bdecf5038e6880e2
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-05-10 17:46:57 +0300

    Added educational background & Referees

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -32,7 +32,31 @@ function Resume() {
             Education Background
           </h1>
           {resume.map((res, index) => (
-            <div className="shadow-md rounded-md h-[100px] border-2 items-center border-gray-200 mb-3 p-3 w-full hover:shadow-lg"></div>
+            <div className="shadow-md rounded-md h-[100px] border-2 items-center border-gray-200 mb-3 p-3 w-full hover:shadow-lg">
+              <div className="flex justify-between">
+                <h2 className="font-semibold">Moringa School</h2>
+                <p className="">Sept 2023 - June 2024</p>
+              </div>
+              <p className="">
+                <b>Certification: </b>Software Engineering
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="">
+        <h2 className="font-bold text-2xl mb-4 text-center">Referees</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+          {resume.map((r, index) => (
+            <div className="h-[100px] w-full mb-2">
+              <div className="flex flex-col">
+                <h3 className="font-semibold">Referee Name</h3>
+                <p className="">someone@email.com</p>
+                <p className="">Company Name</p>
+                <p className="">Company Address</p>
+              </div>
+            </div>
           ))}
         </div>
       </div>
