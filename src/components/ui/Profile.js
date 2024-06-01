97ce6d270ea95713207d6d30f8b5307eb4b27ec3
benny <benny.mathew@student.moringaschool.com>
2024-06-02 00:15:24 +0300

    added github link

--- src/components/ui/Profile.js
+++ src/components/ui/Profile.js
@@ -23,6 +23,15 @@ function Profile() {
           <img src="/linkedin.svg" alt="linkedin" className="h-7 w-7" />
           My LinkedIn
         </a>
+        <a
+          href="https://www.github.com/coderbenny"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex justify-center underline text-center text-white items-center mx-auto"
+        >
+          <img src="/github.svg" alt="linkedin" className="h-7 w-7" />
+          My Github
+        </a>
       </div>
     </div>
   );
