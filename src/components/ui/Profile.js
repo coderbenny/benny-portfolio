8b182be8c554ca261bd1e30b157fad99e5965e68
benny <benny.mathew@student.moringaschool.com>
2024-06-01 14:53:23 +0300

    added profile pictture, updates to styling

--- src/components/ui/Profile.js
+++ src/components/ui/Profile.js
@@ -3,7 +3,11 @@ import React from "react";
 function Profile() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="mt-16 rounded-full w-60 h-60 bg-gray-300 mb-4"></div>
+      <img
+        alt="profile"
+        src="/linkedin_profile.jpeg"
+        className="mt-16 rounded-full w-60 h-60 mb-4"
+      />
       <div className="flex flex-col text-center mb-5">
         <p className="text-3xl tracking-wide text-white">Benny Hinn</p>
         <p className="text-xl text-white">Full Stack Software Engineer</p>
